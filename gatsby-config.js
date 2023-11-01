3b45960fde685043e5113ce9ea195ccf9299c78f
Emil Kowalczyk <emilkowalczyk0@icloud.com>
2023-11-01 19:08:12 +0100

    v0.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `kajaki-kamienczyk`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.kajakikamienczyk.pl`,
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: ['gatsby-plugin-styled-components'],
 };
