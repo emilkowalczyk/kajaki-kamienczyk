f2c5fcd0978d69a54003c31e8d59981a8ea1943e
Emil Kowalczyk <emilkowalczyk0@icloud.com>
2023-11-01 11:08:31 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `kajaki-kamienczyk`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
